f7ec6f45a9bec6a7049b5570e38191245d01ab47
Anthony Manuel Barrera Hidalgo <anthony_barrera@ucne.edu.do>
2018-09-13 11:13:43 -0400

    Metodos Test Probados Exitosamente

--- CotizacionArticulosTests/BLL/ArticuloBLLTests.cs
+++ CotizacionArticulosTests/BLL/ArticuloBLLTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CotizacionArticulos.Entidades;
 
 namespace CotizacionArticulos.BLL.Tests
 {
@@ -14,25 +15,52 @@ namespace CotizacionArticulos.BLL.Tests
         [TestMethod()]
         public void GuardarTest()
         {
-            Assert.Fail();
+            bool paso;
+            Articulo articulo = new Articulo();
+            
+            articulo.FechaVencimiento = DateTime.Now;
+            articulo.Descripcion = "Probando";
+            articulo.Precio = 0.0f;
+            articulo.Existencia = 10f;
+            articulo.CantidadCotizada = 5f;
+            paso = ArticuloBLL.Guardar(articulo);
+            Assert.AreEqual(paso, true);
+
         }
 
         [TestMethod()]
         public void ModificarTest()
         {
-            Assert.Fail();
+            bool paso;
+            Articulo articulo = new Articulo();
+            articulo.FechaVencimiento = DateTime.Now;
+            articulo.Descripcion = "Probando1234";
+            articulo.ArticuloId = 1;
+            articulo.Precio = 0.0f;
+            articulo.Existencia = 10f;
+            articulo.CantidadCotizada = 5f;
+            paso = ArticuloBLL.Modificar(articulo);
+            Assert.AreEqual(paso, true);
         }
 
         [TestMethod()]
         public void EliminarTest()
         {
-            Assert.Fail();
+            bool paso;
+            Articulo articulo = new Articulo();
+            articulo.ArticuloId = 1;
+            paso = ArticuloBLL.Modificar(articulo);
+            Assert.AreEqual(paso, true);
         }
 
         [TestMethod()]
         public void BuscarTest()
         {
-            Assert.Fail();
+            bool paso;
+            Articulo articulo = new Articulo();
+            articulo.ArticuloId = 1;
+            paso = ArticuloBLL.Modificar(articulo);
+            Assert.AreEqual(paso, true);
         }
 
         [TestMethod()]
