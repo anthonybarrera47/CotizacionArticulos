ff8545135a2360fe03847bf5b7ebb9d42c85c3d8
Anthony Manuel Barrera Hidalgo <anthony_barrera@ucne.edu.do>
2018-09-13 11:03:08 -0400

    BLL y entidades Terminados

--- CotizacionArticulos/BLL/ArticuloBLL.cs
+++ CotizacionArticulos/BLL/ArticuloBLL.cs
@@ -1,4 +1,5 @@
-﻿using CotizacionArticulos.Entidades;
+﻿using CotizacionArticulos.DAL;
+using CotizacionArticulos.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,26 +13,111 @@ namespace CotizacionArticulos.BLL
     {
 
         /// <summary>
-        /// 
+        /// Este metodo Permite Guardar un Articulo en la DB
         /// </summary>
         /// <param name="articulo">una instancia de Articulos</param>
         /// <returns>Retorna True si Guardo o Falso si Fallo</returns>
         public static bool Guardar(Articulo articulo)
         {
-
+            bool paso = false;
+            //Creamos una instancia del Contexto para conectar con la base de datos.
+            Contexto db = new Contexto();
+            try
+            {
+                if(db.articulo.Add(articulo)!=null)
+                {
+                    db.SaveChanges();
+                    paso = true;
+                }
+                db.Dispose();
+            }catch(Exception)
+            {
+                throw;
+            }
+            return paso;
         }
+        /// <summary>
+        /// Este metodo permite Modificar un Articulo en la DB
+        /// </summary>
+        /// <param name="articulo">una instancia de Articulos</param>
+        /// <returns>Retorna True si Modifico o Falso si Fallo</returns>
         public static bool Modificar(Articulo articulo)
         {
-
+            bool paso = false;
+            //Creamos una instancia de contexto para conectar con la DB
+            Contexto db = new Contexto();
+            try
+            {
+                db.Entry(articulo).State = System.Data.Entity.EntityState.Modified;
+                if(db.SaveChanges()>0)
+                {
+                    paso = true;
+                }
+                db.Dispose();
+            }catch(Exception)
+            {
+                throw;
+            }
+            return paso;
         }
+        /// <summary>
+        /// E
+        /// </summary>
+        /// <param name="id">El Id que se desea Eliminar</param>
+        /// <returns>Retorna True si Eliminoo Falso si Fallo</returns>
         public static bool Eliminar(int id)
         {
-
+            bool paso = false;
+            //Creamos una instancia de Contexto para conectar con la DB
+            Contexto db = new Contexto();
+            try
+            {
+                Articulo articulo = db.articulo.Find(id);
+                db.articulo.Remove(articulo);
+                if(db.SaveChanges()>0)
+                {
+                    paso = true;
+                }
+                db.Dispose();
+            }catch(Exception)
+            {
+                throw;
+            }
+            return paso;
         }
+        /// <summary>
+        /// Permite Buscar una Entidad En la DB
+        /// </summary>
+        /// <param name="id">El Id del articulo que se desea encontrar</param>
+        /// <returns>Retorna el articulo encontrado</returns>
         public static Articulo Buscar(int id)
         {
-
+            //Creamos una instancia de Contexto para conectar con la DB
+            Contexto db = new Contexto();
+            Articulo articulo = new Articulo();
+            try
+            {
+                articulo = db.articulo.Find(id);
+                db.Dispose();
+            }catch(Exception)
+            {
+                throw;
+            }
+            return articulo;
         }
         public static List<Articulo> GetList(Expression<Func<Articulo,bool>>expression)
+        {
+            List<Articulo> articulos = new List<Articulo>();
+            Contexto db = new Contexto();
+            try
+            {
+                articulos = db.articulo.Where(expression).ToList();
+                db.Dispose();
+            }catch(Exception)
+            {
+                throw;
+            }
+            return articulos;
+        }
     }
 }
---
+++ CotizacionArticulosTests/BLL/ArticuloBLLTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CotizacionArticulos.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotizacionArticulos.BLL.Tests
+{
+    [TestClass()]
+    public class ArticuloBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
